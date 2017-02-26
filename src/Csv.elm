2f66ceb2db20ae6ae5a82e437c9c26f43a91a52d
Ophir LOJKINE <pere.jobs@gmail.com>
2017-02-26 19:05:25 +0300

    Factorize code in two submodules
    Makes it possible to test private functions

--- src/Csv.elm
+++ src/Csv.elm
@@ -9,8 +9,8 @@ module Csv exposing (Csv, parse, parseWith, split, splitWith)
 import List
 import String
 import Maybe
-import Regex
 
+import Helper exposing (..)
 
 {-| The `Csv` type structure.
 -}
@@ -90,54 +90,3 @@ splitWith separator lines =
         List.map (splitLineWith separator) values
 
 
-{-| Split a CSV line, with the traditional comma separator
-
-    >>> splitLine "hello,world"
-    ["hello", "world"]
--}
-splitLine : String -> List String
-splitLine =
-    splitLineWith ","
-
-
-{-| Split a CSV line with the given separator
-
-    >>> splitLineWith " " "a b"
-    ["a", "b"]
-
-    >>> splitLineWith " " "a \"b\""
-    ["a", "b"]
-
-    >>> splitLineWith " " "a \"b c\""
-    ["a", "b c"]
--}
-splitLineWith : String -> String -> List String
-splitLineWith separator line =
-    let
-        values =
-            String.split separator line
-    in
-        List.map (trimQuotes << String.trim) values
-
-
-{-| Extract text from a csv field
-
-  trimQuotes "abc" == "abc"
-  trimQuotes "\"this is a \"\"test\"\" ! \"" == "This is a \"test\" ! "
--}
-trimQuotes : String -> String
-trimQuotes value =
-    let
-        start =
-            String.startsWith "\"" value
-
-        end =
-            String.endsWith "\"" value
-    in
-        if start && end then
-            -- Replace escaped quotes like "" and \"
-            Regex.replace Regex.All (Regex.regex "[\"\\\\]\"") (always "\"") value
-                |> String.dropRight 1
-                |> String.dropLeft 1
-        else
-            value
